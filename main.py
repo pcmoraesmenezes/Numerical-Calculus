8ceeb75df95e4a01bdd33f32888d681a8e51e66a
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-04-14 18:31:16 -0300

    :sparkles: feat: File that contains a call to the functions to find root

---
+++ main.py
@@ -0,0 +1,16 @@
+from func import (
+    bissection,
+    false_position_method,
+    point_fix,
+    newton_raphson,
+    secant_method,
+    fi,
+    derivative_f,
+    f
+)
+
+if __name__ == "__main__":
+    """
+    Main function, tests the implemented methods
+    """
+    ...
